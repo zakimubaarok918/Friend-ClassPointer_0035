d62d9f2cd9081e1334802c1d4dcc8e7884d2e283
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-04-27 13:44:37 +0700

    Refactor 'pelajar' class to include private member 'nilai' and friend function for 'manusia' class

--- friendFunctionDua.cpp
+++ friendFunctionDua.cpp
@@ -7,3 +7,13 @@ class manusia
 public:
     void showNilaiPelajar(pelajar &x);
 };
+
+class pelajar
+{
+private:
+    int nilai;
+
+public:
+    pelajar() { nilai = 100; } 
+    friend void manusia::showNilaiPelajar(pelajar &x);
+};
