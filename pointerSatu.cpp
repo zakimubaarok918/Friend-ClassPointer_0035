14b976ab8756f87bf991185228bd644b69b7e2ea
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-04-27 13:48:39 +0700

    Implement main function to demonstrate 'mahasiswa' object manipulation and pointer usage

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -7,3 +7,17 @@ class mahasiswa{
         cout << "No Induk = " << nim << endl;
     }
 };
+
+int main(){
+    mahasiswa mhs{1}; // Object mhs
+    mhs.showNim(); // Member Access Operator
+
+    mahasiswa &refMhs = mhs; // Pointer Reference refMhs
+    refMhs.nim = 2; // Member Access Operator
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs; // Pointer Dereference pMhs
+    pMhs->nim = 3; // Arrow Operator
+    pMhs->showNim();
+    return 0;
+}
